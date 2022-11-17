b21f96c5178c65d5ebb9db9c8721fdc0c118033c
Mr. Fluffy <git@sixearsm>
2022-11-17 19:32:22 +0000

    upgrade to callPackage-based versions

--- flake.nix
+++ flake.nix
@@ -3,10 +3,10 @@
 
   inputs = {
     nixpkgs.url       = "github:nixos/nixpkgs/be44bf67"; # nixos-22.05 2022-10-15
-    build-utils.url   = "github:sixears/flake-build-utils/r1.0.0.10";
+    build-utils.url   = "github:sixears/flake-build-utils/r1.0.0.11";
 
-    more-unicode.url  = "github:sixears/more-unicode/r0.0.17.7";
-    tasty-plus.url    = "github:sixears/tasty-plus/r1.5.2.10";
+    more-unicode.url  = "github:sixears/more-unicode/r0.0.17.8";
+    tasty-plus.url    = "github:sixears/tasty-plus/r1.5.2.11";
 
   };
 
