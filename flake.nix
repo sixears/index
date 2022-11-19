7c812b88bf2b89b9750f52fb60156a399acde908
Mr. Fluffy <git@sixearsm>
2022-11-19 09:50:13 +0000

    add flake; use ghc-8.10.7 for tfmt

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Class for things that are indexable";
 
   inputs = {
-    nixpkgs.url       = "github:nixos/nixpkgs/be44bf67"; # nixos-22.05 2022-10-15
+    nixpkgs.url       = github:nixos/nixpkgs/be44bf67; # nixos-22.05 2022-10-15
     build-utils.url   = github:sixears/flake-build-utils/r1.0.0.13;
 
     more-unicode.url  = github:sixears/more-unicode/r0.0.17.11;
@@ -12,9 +12,24 @@
   outputs = { self, nixpkgs, build-utils
             , more-unicode, tasty-plus }:
     build-utils.lib.hOutputs self nixpkgs "index" {
-      deps = {
-        inherit more-unicode tasty-plus;
-      };
       ghc = p: p.ghc8107; # for tfmt
+      callPackage = { mkDerivation, lib, mapPkg, system
+                    , base, base-unicode-symbols, hashable, optparse-applicative
+                    , safe, tasty, tasty-hunit, unordered-containers
+                    }:
+        mkDerivation {
+          pname = "index";
+          version = "1.0.1.23";
+          src = ./.;
+          libraryHaskellDepends = [
+            base base-unicode-symbols hashable safe tasty tasty-hunit
+            unordered-containers
+          ] ++ mapPkg [ more-unicode tasty-plus ];
+          testHaskellDepends = [
+            base base-unicode-symbols optparse-applicative
+          ] ++ mapPkg [ more-unicode tasty-plus ];
+          description = "Class for things that are indexable";
+          license = lib.licenses.mit;
+        };
     };
 }
