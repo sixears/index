39bc008b0826535ba284ea9e91b1f601978a6cd7
Mr. Fluffy <git@sixearsm>
2022-11-20 06:53:16 +0000

    exited->1.0.4.22;monaderror-io->1.2.5.19;tfmt->0.2.7.24;number->1.1.2.14;has-callstack->1.0.1.18;base0->0.0.4.11;more-unicode->0.0.17.12;quasiquoting->1.0.1.31

--- flake.nix
+++ flake.nix
@@ -5,8 +5,8 @@
     nixpkgs.url       = github:nixos/nixpkgs/be44bf67; # nixos-22.05 2022-10-15
     build-utils.url   = github:sixears/flake-build-utils/r1.0.0.13;
 
-    more-unicode.url  = github:sixears/more-unicode/r0.0.17.11;
-    tasty-plus.url    = github:sixears/tasty-plus/r1.5.2.22;
+    more-unicode.url  = github:sixears/more-unicode/r0.0.17.12;
+    tasty-plus.url    = github:sixears/tasty-plus/r1.5.2.23;
   };
 
   outputs = { self, nixpkgs, build-utils
@@ -19,7 +19,7 @@
                     }:
         mkDerivation {
           pname = "index";
-          version = "1.0.1.24";
+          version = "1.0.1.25";
           src = ./.;
           libraryHaskellDepends = [
             base base-unicode-symbols hashable safe tasty tasty-hunit
